e96f8cb6c2c7bac0703dd6f962b352f46b13932a
seakjh <skykjh0513@gmail.com>
2020-03-20 10:46:47 +0900

    생명주기 코드 예시

--- app/src/main/java/com/example/a20200320_01_profilesetting/MainActivity.java
+++ app/src/main/java/com/example/a20200320_01_profilesetting/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.a20200320_01_profilesetting.databinding.ActivityMainBinding;
 
@@ -17,6 +18,12 @@ public class MainActivity extends BaseActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setupEvents();
         setValues();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("화면 재등장", "다시 나타날때마다 실행");
     }
 
     @Override
